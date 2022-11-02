a1a85d8fdca094b54ba5cb5803dccabec36c062c
Jonathan Montrose <64240200+jem0ntr053@users.noreply.github.com>
2022-11-02 13:24:40 -0500

    added things

---
+++ mbed-os-simple-timeout/main.cpp
@@ -0,0 +1,18 @@
+#include "mbed.h"
+ 
+#define TIME 1                              // Time for toggle the LED (seconds)
+ 
+DigitalOut led(D3);                    // Create the LED object and setup OFF
+Timeout timeout;                            // Create the Timeout object
+    
+void timer_interrupt()
+{
+    led = !led;                             // Toggle the LED state
+    timeout.attach(&timer_interrupt, 2s); // Set again the timer timeout for next iterations
+}
+ 
+int main()
+{    
+    timeout.attach(&timer_interrupt, 2s); // Set the timer interrupt service rutine (ISR) and the time for the timeout (in seconds)
+    while(1);                               // Infinite loop
+}
